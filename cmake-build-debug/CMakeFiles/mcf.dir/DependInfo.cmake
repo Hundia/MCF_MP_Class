943d20c4d7dcb099dc336a39a51d8b7c18747634
Laptop\Hundia <eli.hundia@gmail.com>
2018-04-09 21:56:47 +0200

    Added cmake stuff as well

---
+++ cmake-build-debug/CMakeFiles/mcf.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "D:/MSC/MultiThreading/mcf/implicit.c" "D:/MSC/MultiThreading/mcf/cmake-build-debug/CMakeFiles/mcf.dir/implicit.c.obj"
+  "D:/MSC/MultiThreading/mcf/mcf.c" "D:/MSC/MultiThreading/mcf/cmake-build-debug/CMakeFiles/mcf.dir/mcf.c.obj"
+  "D:/MSC/MultiThreading/mcf/mcfutil.c" "D:/MSC/MultiThreading/mcf/cmake-build-debug/CMakeFiles/mcf.dir/mcfutil.c.obj"
+  "D:/MSC/MultiThreading/mcf/output.c" "D:/MSC/MultiThreading/mcf/cmake-build-debug/CMakeFiles/mcf.dir/output.c.obj"
+  "D:/MSC/MultiThreading/mcf/pbeampp.c" "D:/MSC/MultiThreading/mcf/cmake-build-debug/CMakeFiles/mcf.dir/pbeampp.c.obj"
+  "D:/MSC/MultiThreading/mcf/pbla.c" "D:/MSC/MultiThreading/mcf/cmake-build-debug/CMakeFiles/mcf.dir/pbla.c.obj"
+  "D:/MSC/MultiThreading/mcf/pflowup.c" "D:/MSC/MultiThreading/mcf/cmake-build-debug/CMakeFiles/mcf.dir/pflowup.c.obj"
+  "D:/MSC/MultiThreading/mcf/psimplex.c" "D:/MSC/MultiThreading/mcf/cmake-build-debug/CMakeFiles/mcf.dir/psimplex.c.obj"
+  "D:/MSC/MultiThreading/mcf/pstart.c" "D:/MSC/MultiThreading/mcf/cmake-build-debug/CMakeFiles/mcf.dir/pstart.c.obj"
+  "D:/MSC/MultiThreading/mcf/readmin.c" "D:/MSC/MultiThreading/mcf/cmake-build-debug/CMakeFiles/mcf.dir/readmin.c.obj"
+  "D:/MSC/MultiThreading/mcf/treeup.c" "D:/MSC/MultiThreading/mcf/cmake-build-debug/CMakeFiles/mcf.dir/treeup.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/mcf.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mcf.dir/implicit.c.obj"
+  "CMakeFiles/mcf.dir/mcf.c.obj"
+  "CMakeFiles/mcf.dir/mcfutil.c.obj"
+  "CMakeFiles/mcf.dir/output.c.obj"
+  "CMakeFiles/mcf.dir/pbeampp.c.obj"
+  "CMakeFiles/mcf.dir/pbla.c.obj"
+  "CMakeFiles/mcf.dir/pflowup.c.obj"
+  "CMakeFiles/mcf.dir/psimplex.c.obj"
+  "CMakeFiles/mcf.dir/pstart.c.obj"
+  "CMakeFiles/mcf.dir/readmin.c.obj"
+  "CMakeFiles/mcf.dir/treeup.c.obj"
+  "../bin/mcf.pdb"
+  "../bin/mcf.exe"
+  "../bin/mcf.exe.manifest"
+  "libmcf.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/mcf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
