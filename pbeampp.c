71e9f3c8e097457b20458f2cc8ecc188262b2f9d
Laptop\Hundia <eli.hundia@gmail.com>
2018-08-08 03:05:24 +0200

    gg

--- pbeampp.c
+++ pbeampp.c
@@ -1,3 +1,6 @@
+#define OLD_CODE
+#ifdef NEW_CODE
+
 /**************************************************************************
 PBEAMPP.C of ZIB optimizer MCF, SPEC version
 
@@ -142,8 +145,7 @@ arc_t *primal_bea_mpp( m, arcs, stop_arcs, red_cost_of_bea )
         basket_size = 0;
         initialize = 0;
     }
-#define NEW_CODE
-#ifdef NEW_CODE
+
     else
     {
         //  Used to perserve the red cost condition result
@@ -177,11 +179,7 @@ arc_t *primal_bea_mpp( m, arcs, stop_arcs, red_cost_of_bea )
         #pragma omp parallel for num_threads(NUM_OF_THREADS)
         for (int j = 0; j < NUM_OF_THREADS; j++)
         {
-//            int tid = omp_get_thread_num();
-//            printf("Hello World from thread = %d\n", tid);
-//            if(j == NUM_OF_THREADS - 1) {
-//                samples_per_thread = minimum_size_of_iteration;
-//            }
+
             //  Now each thread needs to run on his own samples.
             //  The original loop started its iteration at 2, so we
             //  need to calculate each thread start and end interval.
@@ -209,14 +207,9 @@ arc_t *primal_bea_mpp( m, arcs, stop_arcs, red_cost_of_bea )
                     //  Sum up the number of times the condition was met
                     ResultsPerThread[j] += 1;
                 }
-                else {
-                    ConditionResultsArray[k] = 0;
-                }
             }
-
         }
 
-//        printf("end parralized first loop and starting second\n");
         #pragma omp parallel for num_threads(NUM_OF_THREADS)
         for (int j = 0; j < NUM_OF_THREADS; j++)
         {
@@ -246,7 +239,8 @@ arc_t *primal_bea_mpp( m, arcs, stop_arcs, red_cost_of_bea )
                     CurrentThreadResultPositionTracker++;
                     //  Result position is the starting position +
                     int ResultPosition = ThreadResultStartingPosition + CurrentThreadResultPositionTracker;
-
+//                    if(j==1)
+//                        printf("ResultPosition: %d, CurrentThreadResultPositionTracker: %d \n",ResultPosition,CurrentThreadResultPositionTracker);
                     //  Write the results
                     perm[ResultPosition]->a = resArr[k].arc;
                     perm[ResultPosition]->cost = resArr[k].red_cost;
@@ -255,17 +249,214 @@ arc_t *primal_bea_mpp( m, arcs, stop_arcs, red_cost_of_bea )
             }
 
             //  Now set the basket size to be the sum of all results
-            basket_size = 0;
             for (int l = 0; l < NUM_OF_THREADS; ++l) {
                 basket_size = ResultsPerThread[l];
             }
         }
-
-//        printf("Ended second parralized loop\n");
-
-        basket_size = next;
     }
+
+
+NEXT:
+    /* price next group */
+    arc = arcs + group_pos;
+    for( ; arc < stop_arcs; arc += nr_group )
+    {
+        if( arc->ident > BASIC )
+        {
+            /* red_cost = bea_compute_red_cost( arc ); */
+            red_cost = arc->cost - arc->tail->potential + arc->head->potential;
+            if( bea_is_dual_infeasible( arc, red_cost ) )
+            {
+                basket_size++;
+                perm[basket_size]->a = arc;
+                perm[basket_size]->cost = red_cost;
+                perm[basket_size]->abs_cost = ABS(red_cost);
+            }
+        }
+
+    }
+
+    if( ++group_pos == nr_group )
+        group_pos = 0;
+
+    if( basket_size < B && group_pos != old_group_pos )
+        goto NEXT;
+
+    if( basket_size == 0 )
+    {
+        initialize = 1;
+        *red_cost_of_bea = 0;
+        return NULL;
+    }
+
+    sort_basket( 1, basket_size );
+
+    *red_cost_of_bea = perm[1]->cost;
+    return( perm[1]->a );
+}
 #endif
+
+#ifdef OLD_CODE
+/**************************************************************************
+PBEAMPP.C of ZIB optimizer MCF, SPEC version
+
+This software was developed at ZIB Berlin. Maintenance and revisions
+solely on responsibility of Andreas Loebel
+
+Dr. Andreas Loebel
+Ortlerweg 29b, 12207 Berlin
+
+Konrad-Zuse-Zentrum fuer Informationstechnik Berlin (ZIB)
+Scientific Computing - Optimization
+Takustr. 7, 14195 Berlin-Dahlem
+
+Copyright (c) 1998-2000 ZIB.
+Copyright (c) 2000-2002 ZIB & Loebel.
+Copyright (c) 2003-2005 Andreas Loebel.
+**************************************************************************/
+/*  LAST EDIT: Sun Nov 21 16:22:04 2004 by Andreas Loebel (boss.local.de)  */
+/*  $Id: pbeampp.c,v 1.10 2005/02/17 19:42:32 bzfloebe Exp $  */
+
+
+
+#define K 300
+#define B  50
+
+
+
+#include "pbeampp.h"
+
+
+
+
+#ifdef _PROTO_
+int bea_is_dual_infeasible( arc_t *arc, cost_t red_cost )
+#else
+int bea_is_dual_infeasible( arc, red_cost )
+    arc_t *arc;
+    cost_t red_cost;
+#endif
+{
+    return(    (red_cost < 0 && arc->ident == AT_LOWER)
+            || (red_cost > 0 && arc->ident == AT_UPPER) );
+}
+
+
+
+
+
+
+
+typedef struct basket
+{
+    arc_t *a;
+    cost_t cost;
+    cost_t abs_cost;
+} BASKET;
+
+static long basket_size;
+static BASKET basket[B+K+1];
+static BASKET *perm[B+K+1];
+
+
+
+#ifdef _PROTO_
+void sort_basket( long min, long max )
+#else
+void sort_basket( min, max )
+    long min, max;
+#endif
+{
+    long l, r;
+    cost_t cut;
+    BASKET *xchange;
+
+    l = min; r = max;
+
+    cut = perm[ (long)( (l+r) / 2 ) ]->abs_cost;
+
+    do
+    {
+        while( perm[l]->abs_cost > cut )
+            l++;
+        while( cut > perm[r]->abs_cost )
+            r--;
+
+        if( l < r )
+        {
+            xchange = perm[l];
+            perm[l] = perm[r];
+            perm[r] = xchange;
+        }
+        if( l <= r )
+        {
+            l++; r--;
+        }
+
+    }
+    while( l <= r );
+
+    if( min < r )
+        sort_basket( min, r );
+    if( l < max && l <= B )
+        sort_basket( l, max );
+}
+
+
+
+
+
+
+static long nr_group;
+static long group_pos;
+
+
+static long initialize = 1;
+
+
+#ifdef _PROTO_
+arc_t *primal_bea_mpp( long m,  arc_t *arcs, arc_t *stop_arcs,
+                              cost_t *red_cost_of_bea )
+#else
+arc_t *primal_bea_mpp( m, arcs, stop_arcs, red_cost_of_bea )
+    long m;
+    arc_t *arcs;
+    arc_t *stop_arcs;
+    cost_t *red_cost_of_bea;
+#endif
+{
+    long i, next, old_group_pos;
+    arc_t *arc;
+    cost_t red_cost;
+
+    if( initialize )
+    {
+        for( i=1; i < K+B+1; i++ )
+            perm[i] = &(basket[i]);
+        nr_group = ( (m-1) / K ) + 1;
+        group_pos = 0;
+        basket_size = 0;
+        initialize = 0;
+    }
+    else
+    {
+        for( i = 2, next = 0; i <= B && i <= basket_size; i++ )
+        {
+            arc = perm[i]->a;
+            red_cost = arc->cost - arc->tail->potential + arc->head->potential;
+            if( (red_cost < 0 && arc->ident == AT_LOWER)
+                || (red_cost > 0 && arc->ident == AT_UPPER) )
+            {
+                next++;
+                perm[next]->a = arc;
+                perm[next]->cost = red_cost;
+                perm[next]->abs_cost = ABS(red_cost);
+            }
+                }
+        basket_size = next;
+        }
+
+    old_group_pos = group_pos;
 
 NEXT:
     /* price next group */
@@ -306,8 +497,7 @@ NEXT:
     return( perm[1]->a );
 }
 
-
-
+#endif
 
 
 
